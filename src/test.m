d2a5a6121d8adb524e5b62c73c051588ca55e553
YuanYouYaun <az6980522@gmail.com>
2016-11-21 15:44:32 +0800

    update matlab code

--- src/test.m
+++ src/test.m
@@ -1,13 +1,24 @@
 close all;
 clear all;
-load gait-test.mat % load time, theta
 
-%time = [0 2 4 7 10];
-%theta = [0 pi/2 pi/5 pi/3 pi/4;
-%		 0 pi/4 pi/3 pi/3 pi/2;
-%		 0 pi/3 pi/6 pi/2 pi/4];
+%time = [0 1 3];
+%theta = [30  45 60;
+%		 120 90 60;
+%		 30  45 60];
+
+load data/test_flip_gait
+%load data/old_flip_gait
+%theta = [-90 0 90;
+%		0 90 90;
+%		-90 0 90];
+%time = [0 1 2]
+
 %[th, dth, ddth, t] = joint_trajectory(theta, time, 0);
 %torq = torque(th, dth, ddth);
 
-exceed = torque_exceed(theta, time, 1);
+min_time = find_min_time(theta, time)
+torque_exceed(theta, min_time, 1, 1);
+
+
+%exceed = torque_exceed(theta, time, 0);
 
