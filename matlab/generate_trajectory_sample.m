565dad30193b31b17cc9a41397d81c76785aad1f
YuanYouYaun <az6980522@gmail.com>
2016-12-05 01:28:19 +0800

    update README.md

--- matlab/generate_trajectory_sample.m
+++ matlab/generate_trajectory_sample.m
@@ -4,3 +4,6 @@ addpath('./function')
 load('./data/gaits/stand_up_gait.mat'); % load theta, time
 plot_figure = 1;
 [th, dth, ddth] = joint_trajectory(theta, time, plot_figure);
+
+th = th*180/pi;
+csvwrite('./data/gaits/stand_up_gait.csv', th);
