e2408eef13fbddf8e18b5786b2a94995977a0fcc
YuanYouYaun <az6980522@gmail.com>
2016-12-05 00:16:43 +0800

    extend joint_trajectory.m to n motors

--- matlab/function/old_joint_trajectory.m
+++ matlab/function/old_joint_trajectory.m
@@ -1,9 +1,8 @@
 function [th, dth, ddth, t] = joint_trajectory(theta, time, plot_figure)
 	theta = theta*pi/180;
 	t = time(1):0.01:time(end);
-    n = size(theta, 1);
 
-	for i = 1:n
+	for i = 1:size(theta,1)
 
 		% spline_th   = spline(time, [0 theta(i,:) 0]); 
 		% clamped boundary condition with initail/final velocity = 0
@@ -22,25 +21,25 @@ function [th, dth, ddth, t] = joint_trajectory(theta, time, plot_figure)
 
 	if exist('plot_figure', 'var') && plot_figure == 1
 		figure('Name', 'Joint Trajectory');
-		for i = 1:n
+		for i = 1:size(theta,1)
 			grid on;
-			subplot(3, n, i);
-			plot(time, theta(i, :)*180/pi, 'o'); hold on;
-			plot(t, th(i,:)*180/pi); hold on;
+			subplot(3, 3, i);
+			plot(time, theta(i, :), 'o'); hold on;
+			plot(t, th(i,:)); hold on;
 			title(['Joint' num2str(i)]);
 			xlabel('time(s)');
-			ylabel('\theta(deg)', 'FontSize', 14);
+			ylabel('\theta(rad)', 'FontSize', 14);
 			set(gca, 'xtick', time);
 			grid on;
 		
-			subplot(3, n, i+n);
+			subplot(3, 3, i+3);
 			plot(t, dth(i,:)), hold on;
 			xlabel('time(s)');
 			ylabel('\omega(rad/s)  ', 'FontSize', 14);
 			set(gca, 'xtick', time);
 			grid on;
 		
-			subplot(3, n, i+2*n);
+			subplot(3, 3, i+6);
 			plot(t, ddth(i,:)), hold on;
 			xlabel('time(s)');
 			ylabel('\alpha(rad/s^{2})', 'FontSize', 14);
--- matlab/generate_trajectory_sample.m
+++ matlab/generate_trajectory_sample.m
@@ -1,6 +1,6 @@
 close all;
 clear all;
 addpath('./function')
-load('./data/gaits/test_flip_gait.mat'); % load theta, time
+load('./data/gaits/stand_up_gait.mat'); % load theta, time
 plot_figure = 1;
 [th, dth, ddth] = joint_trajectory(theta, time, plot_figure);
