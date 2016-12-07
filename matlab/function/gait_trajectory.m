35d6fdc27d44dff5e93c8ecf2109a4a488f5d675
YuanYouYuan <az6980522@gmail.com>
2016-12-08 00:15:20 +0800

    add one step gait and climb up gait

--- matlab/function/gait_trajectory.m
+++ matlab/function/gait_trajectory.m
@@ -1,11 +1,18 @@
-function [] = gait_trajectory(gait_name, file_name)
+function [] = gait_trajectory(gait_name, plot_fig, file_name)
     if ~exist('file_name', 'var')
         file_name = gait_name;
     end
+    
+    if ~exist('plot_fig', 'var')
+        plot_fig = 0;
+    end
+    
     load(['./data/gaits/' gait_name '.mat']); % load theta, time
-    plot_figure = 1;
-    [th, dth, ddth] = joint_trajectory(theta, time, plot_figure);
+    [th, dth, ddth] = joint_trajectory(theta, time, plot_fig);
     th = th*180/pi;
     csvwrite(['./data/gaits/csv/' file_name '.csv'], th);
-    saveas(gcf, ['./data/gaits/traj/' file_name '.png']);
+    csvwrite(['R:\c\' file_name '.csv'], th);
+    if plot_fig == 1
+        saveas(gcf, ['./data/gaits/traj/' file_name '.png']);
+    end
 end
