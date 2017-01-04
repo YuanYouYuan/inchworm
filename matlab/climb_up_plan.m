c550e9d6f8e9dfbff0c6bf151c5891fce328c5f0
yuanyouyuan <az6980522@gmail.com>
2017-01-05 01:32:56 +0800

    update climb up plan

--- matlab/climb_up_plan.m
+++ matlab/climb_up_plan.m
@@ -9,14 +9,14 @@ fixed_point.is_switch = 0;
 y_climb_up = 0.4;
 line_number_climb = 11;
 line_number_pull = 10;
-angle_trial = deg2rad(5);
+angle_trial = deg2rad(15);
 angle_in = deg2rad(-90)+angle_trial;
 
 x = [0.3621 0.36 0.3 0.2];
 [~,flip_number] = size(x);
 x_climb_touch = L(1)*sin(angle_trial);
 x_line = [linspace(0.1,x_climb_touch,line_number_climb-1) -0.05 ...
-          linspace(0.35,0.3,line_number_pull)];
+          linspace(0.48,0.4,line_number_pull)];
 x = [x x_line]';
 x = x-fixed_point.x;
 
@@ -44,8 +44,8 @@ joint_2 = 90*ones(1,total_number);
 joint_6 = 90*ones(1,total_number);
 joint_7 = [90*ones(1,flip_number-2) -90*ones(1,line_number_climb+2) -90*ones(1,line_number_pull)];
 theta = [joint_1;joint_2;th;joint_6;joint_7];
-time = [1 1.5 2 3 linspace(4,4.2,line_number_climb-1) 4.3 linspace(4.31,5,line_number_pull)];
+time = [1 1.5 2 3 linspace(4,4.2,line_number_climb) linspace(4.25,5,line_number_pull)];
 
-save('./data/gaits/climb_up_plan.mat','theta','time','name','fixed_point');
+save('./data/gaits/climb_up_plan_fast_bigangle.mat','theta','time','name','fixed_point');
 
 
